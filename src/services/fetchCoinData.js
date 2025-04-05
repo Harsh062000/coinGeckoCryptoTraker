033c7cd86cbbe4bda950903255e2a5110e3c5cac
Harsh Gohil <gohil.harsh06@gmail.com>
2025-04-05 15:22:48 +0530

    Added axios config

---
+++ src/services/fetchCoinData.js
@@ -0,0 +1,14 @@
+import axiosInstance from "../helpers/axiosInstances";
+
+export async function fetchCoinData() {
+       try {
+
+              const response = await axiosInstance.get('/coins/markets?vs_currency=inr');
+              console.log(response);
+              return response;
+
+       } catch (error) {
+              console.log(error);
+              return null;
+       }
+}
