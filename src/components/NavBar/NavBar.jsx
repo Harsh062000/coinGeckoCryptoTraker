e07e07ca85de41829114833e087f5dbe917ec443
Harsh Gohil <gohil.harsh06@gmail.com>
2025-04-12 15:21:57 +0530

    Fix: Added redirect to home page from navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 // import { useContext } from "react"
 // import { CurrencyContext } from "../../context/currencyContext"
+import { useNavigate } from 'react-router-dom';
 import currencyStore from '../../store/store';
 
 function NavBar() {
@@ -7,6 +8,12 @@ function NavBar() {
        // const { setCurrency } = useContext(CurrencyContext);
 
        const { setCurrency } = currencyStore();
+
+       const navigate = useNavigate();
+
+       function redirectToHome() {
+              navigate('/');
+       }
 
        return (
               <div className="navbar bg-base-100 shadow-sm">
@@ -23,20 +30,10 @@ function NavBar() {
                                    </ul>
                             </div>
                      </div>
-                     <div className="navbar-center">
+                     <div onClick={redirectToHome} className="navbar-center">
                             <a className="btn btn-ghost text-xl">Crypto Tracker</a>
                      </div>
-                     <div className="navbar-end">
-                            <button className="btn btn-ghost btn-circle">
-                                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /> </svg>
-                            </button>
-                            <button className="btn btn-ghost btn-circle">
-                                   <div className="indicator">
-                                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /> </svg>
-                                          <span className="badge badge-xs badge-primary indicator-item"></span>
-                                   </div>
-                            </button>
-                     </div>
+
               </div>
        )
 }
